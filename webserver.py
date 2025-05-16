1cf417ccbcee335e263e1bf081e36330cbec68bd
yoksejdr <164357611+yoksejdr@users.noreply.github.com>
2025-05-16 19:32:36 +0200

    Initial

---
+++ webserver.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask("WebIraide")
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/example")
+def example_route():
+    return "Esto es una página de ejemplo"
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port='5000' )
